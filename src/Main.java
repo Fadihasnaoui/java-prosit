8bed2fd7dde0e6f7c0e5fdad2ea2675e0a0826b2
fadi <fadi.hasnaoui@esprit.tn>
2024-09-29 21:46:35 +0100

    prost 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
 public class Main {
     public static void main(String[] args) {
 
-        Animal lion = new Animal("Felidae", "Lion", 5, true);
-
-        Animal[] animals = new Animal[25];
-        animals[0] = lion;
-
-        Zoo myZoo = new Zoo("Safari Zoo", "Paris", 5, animals);
-
-        System.out.println(myZoo);
+        Zoo zoo1 = new Zoo("Safari Park", "Paris");
+        Zoo zoo2 = new Zoo("Jungle Park", "Lyon");
 
 
-        System.out.println(lion);
+        Animal lion = new Animal("Felidae", "Simba", 5, true);
+        Animal tiger = new Animal("Felidae", "Shere Khan", 4, true);
+        Animal giraffe = new Animal("Giraffidae", "Melman", 7, false);
+        Animal elephant = new Animal("Elephantidae", "Dumbo", 10, false);
+
+
+        zoo1.addAnimal(lion);
+        zoo1.addAnimal(tiger);
+        zoo1.addAnimal(giraffe);
+
+
+        zoo2.addAnimal(elephant);
+
+
+        zoo1.displayAnimals();
+        zoo2.displayAnimals();
+
+
+        zoo1.removeAnimal(tiger);
+        zoo1.displayAnimals();
+
+
+        System.out.println("Le zoo 1 est plein ? " + zoo1.isZooFull());
+
+
+        Zoo biggerZoo = Zoo.comparerZoo(zoo1, zoo2);
+        if (biggerZoo != null) {
+            System.out.println("Le zoo avec le plus d'animaux est : " + biggerZoo.name);
+        } else {
+            System.out.println("Les deux zoos ont le même nombre d'animaux.");
+        }
     }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -1,14 +1,85 @@
 public class Zoo {
+    // Attributs
     Animal[] animals;
     String name;
     String city;
-    int nbrCages;
+    int nbrCages = 25;
+    int animalCount;
 
-    public Zoo(String name, String city, int nbrCages, Animal[] animals) {
+    // Constructeur
+    public Zoo(String name, String city) {
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
-        this.animals = animals;
+        this.animals = new Animal[nbrCages];
+        this.animalCount = 0;
+    }
+
+
+    public boolean addAnimal(Animal animal) {
+        if (isZooFull()) {
+            System.out.println("Le zoo est plein, impossible d'ajouter de nouveaux animaux.");
+            return false;
+        }
+
+        if (searchAnimal(animal.name) != -1) {
+            System.out.println("Cet animal existe déjà dans le zoo.");
+            return false;
+        }
+
+        animals[animalCount] = animal;
+        animalCount++;
+        return true;
+    }
+
+
+    public void displayAnimals() {
+        System.out.println("Les animaux du zoo " + name + " :");
+        for (int i = 0; i < animalCount; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+
+
+    public int searchAnimal(String name) {
+        for (int i = 0; i < animalCount; i++) {
+            if (animals[i].name.equals(name)) {
+                return i;
+            }
+        }
+        return -1; // Si l'animal n'est pas trouvé
+    }
+
+    // Méthode pour supprimer un animal du zoo
+    public boolean removeAnimal(Animal animal) {
+        int index = searchAnimal(animal.name);
+        if (index == -1) {
+            return false; // Animal non trouvé
+        }
+
+        // Déplacer les animaux pour combler le trou
+        for (int i = index; i < animalCount - 1; i++) {
+            animals[i] = animals[i + 1];
+        }
+
+        animals[animalCount - 1] = null;
+        animalCount--;
+        return true;
+    }
+
+    // Méthode pour vérifier si le zoo est plein
+    public boolean isZooFull() {
+        return animalCount >= nbrCages;
+    }
+
+
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.animalCount > z2.animalCount) {
+            return z1;
+        } else if (z1.animalCount < z2.animalCount) {
+            return z2;
+        } else {
+            return null;
+        }
     }
 
     @Override
@@ -17,6 +88,7 @@ public class Zoo {
                 "name='" + name + '\'' +
                 ", city='" + city + '\'' +
                 ", nbrCages=" + nbrCages +
+                ", animalCount=" + animalCount +
                 '}';
     }
 }
